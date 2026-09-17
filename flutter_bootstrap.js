4d1f5bfafa97a9fd09f1786fc5df49477b25b5d9
mostafay <gqwxgwx3@gmail.com>
2026-09-17 22:39:25 +0000

    Add changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "275207926" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3595905293" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
